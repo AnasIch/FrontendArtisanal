54d7a4bec7c98e13d95d12ae034bb50a2bcebe6f
Anasich <146490943+AnasIch@users.noreply.github.com>
2025-02-14 17:47:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Homme_page from "./Components/Hommepage/Homme_page"
 import ContactUs from './Components/ContactUs/Contuct_Us';
+import AboutUs from './Components/PeapleRate/PeapleRate/AboutUs'
+import Footer from "./Components/Footer/Footer/Footer"
 import { BrowserRouter as Router ,Routes,Route } from 'react-router-dom';
 // import Footer from './Components/Footer/Footer';
 // import AboutUs from "./Components/PeapleRate/AboutUs"
@@ -23,10 +25,10 @@ function App() {
                 <Route path="/" element={ <Homme_page/>} />
                 <Route path="/Contact_Us" element={ <ContactUs/>} />
             </Routes>
-              {/* <AboutUs/> */}
+              <AboutUs/>
 
               {/* the footer  */}
-              {/* <Footer/> */}
+              <Footer/>
 
 
              
